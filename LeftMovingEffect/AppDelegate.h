7cc445ac506578f1f81c983dd14b1bf33df4f800
Seungpill Baik <seungpillbaik@Seungpill-Baik-ui-MacBook-Pro.local>
2014-11-26 21:12:55 +0900

    Initial Commit

---
+++ LeftMovingEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeftMovingEffect
+//
+//  Created by Seungpill Baik on 2014. 11. 26..
+//  Copyright (c) 2014년 retix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
